078dec6cbe06665088225523273bb31854b16dfc
SampleText2k77 <gutyra13@gmail.com>
2021-04-30 11:29:08 +0300

    Added absent prev variable assignment. Fixed duplicated normal vector and length variable names

--- Code/Chapter-2/convex.cpp
+++ Code/Chapter-2/convex.cpp
@@ -1,7 +1,7 @@
 bool isConvex ( const glm::vec2 v [], int n )
 {
     glm::vec2 prev = v [n-1];
-    glm::vec2 cur, dir, n;
+    glm::vec2 cur, dir, nv;
     float     d, dp;
     int       sign, s;
 
@@ -10,24 +10,26 @@ bool isConvex ( const glm::vec2 v [], int n )
         cur  = v [i];
         dir  = cur - prev;                 // edge direction vector
         n    = glm::vec2 ( dir.y, -dir.x );// normal to edge [prev, cur]
-        d    = -glm::dotdot ( n, cur );    // so line's equation is (p,n) + d = 0
+        d    = -glm::dotdot ( nv, cur );    // so line's equation is (p,n) + d = 0
         sign = 0;                          // unknown yet
 
         for ( int j = 0; j < n; j++ )
         {
-            dp = d + glm::dot ( n, v [j] );
+            dp = d + glm::dot ( nv, v [j] );
 
             if ( fabs ( dp ) < EPS )       // too small
                continue;
-				
+
             s = dp > 0 ? 1 : -1;           // sign for v [j]
-			
+
             if ( sign == 0 )
                sign = s;
             else
             if ( sign != s )
                 return false;
         }
+
+        prev = cur;
     }
 
     return true;
